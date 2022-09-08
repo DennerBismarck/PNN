6da0d5c08cbd9e9cba35462245c6bca3ae701a71
Alec Yalcin <alec.yalcin@escolar.ifrn.edu.br>
2022-09-08 12:02:16 -0300

    Criado Tabela de: 1. Necessitados 2. Situações 3. Profissões 4. Gêneros 5. Cidades 6. Estados

--- Aplicativo/admin.py
+++ Aplicativo/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from Aplicativo     import models
 
 # Register your models here.
+admin.site.register(models.Estado)
+admin.site.register(models.Cidade)
 admin.site.register(models.Profissao)
 admin.site.register(models.Situacao)
 admin.site.register(models.Genero)
--- Aplicativo/models.py
+++ Aplicativo/models.py
@@ -1,12 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Estado(models.Model):
+    est_id = models.AutoField(primary_key=True)
+    est_estado = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.est_estado
+
+class Cidade(models.Model):
+    cid_id = models.AutoField(primary_key=True)
+    cid_cidade = models.CharField(max_length=50)
+    cid_est_id = models.ForeignKey(Estado, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.cid_cidade
+
 class Profissao(models.Model):
     pro_id = models.AutoField(primary_key=True)
     pro_profissao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.pro_profissao
+
+    class Meta:
+        verbose_name        = "Profissão"
+        verbose_name_plural = "Profissões"
 
 class Situacao(models.Model):
     sit_id = models.AutoField(primary_key=True)
@@ -15,12 +34,19 @@ class Situacao(models.Model):
     def __str__(self):
         return self.sit_situacao
 
+    class Meta:
+        verbose_name        = "Situação"
+        verbose_name_plural = "Situações"
+
 class Genero(models.Model):
     gen_id = models.AutoField(primary_key=True)
     gen_generos = models.CharField(max_length=20)
 
     def __str__(self):
         return self.gen_generos
+
+    class Meta:
+        verbose_name        = "Gênero"
 
 class Necessitado(models.Model):
     nec_id          = models.AutoField(primary_key=True)
@@ -32,6 +58,10 @@ class Necessitado(models.Model):
     nec_sit_id      = models.ForeignKey(Situacao, on_delete=models.CASCADE, default=1)
     nec_pro_id      = models.ForeignKey(Profissao, on_delete=models.CASCADE, default=1)
     nec_gen_id      = models.ForeignKey(Genero, on_delete=models.CASCADE, default=1)
+    nec_cid_id      = models.ForeignKey(Cidade, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.nec_nome
+
+    class Meta:
+        verbose_name        = "Necessitado"
